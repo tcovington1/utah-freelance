6d2830efe1020fecf65de4f218d059b3a54fe1ff
Taylor Covington <tcovingtondev@gmail.com>
2020-03-31 15:15:14 -0600

    connected app to MongoDB

--- server.js
+++ server.js
@@ -2,19 +2,25 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const logger = require('./middleware/logger.middleware')
+const connectDB = require('./config/db')
+
+// Load env vars
+dotenv.config({
+  path: './config/config.env'
+});
+
+//connect to db.js file
+connectDB();
+
 
 // we are bringing in the router files to use
 const freelancers = require('./routes/freelancers.route')
 
-dotenv.config({
-  path: './config/config.env'
-})
-
 const app = express();
- 
+
 // Dev logging middleware
 // only want to run if you're in the development env
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
   //using morgan here instead of our own logger because it shows us more information like server status and time
 }
@@ -28,4 +34,11 @@ app.use('/api/v1/freelancers', freelancers)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1))
+})
