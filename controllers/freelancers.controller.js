57d3ecdf9b68d42219366be9149c07525fc962a2
Taylor Covington <tcovingtondev@gmail.com>
2020-03-31 21:41:33 -0600

    All CRUD working for Freelancers. Added update and delete freelancer

--- controllers/freelancers.controller.js
+++ controllers/freelancers.controller.js
@@ -3,23 +3,50 @@ const Freelancer = require('../models/Freelaner.model')
 // @desc Get all freelancers
 // @route GET /api/v1/freelancers
 // @access Public (don't need a token)
-exports.getFreelancers = (req, res, next) => {
+exports.getFreelancers = async (req, res, next) => {
   // whatever we want to do go here
-  res.status(200).json({
-    success: true,
-    msg: `Show all freelancers in Utah.`
-  })
+  try {
+    const freelancers = await Freelancer.find();
+
+    res.status(200).json({
+      success: true,
+      count: freelancers.length,
+      data: freelancers
+    })
+  } catch (error) {
+    res.status(400).json({
+      success: false
+    })
+  }
 }
 
 // @desc Get single freelancer
 // @route GET /api/v1/freelancers/:id
 // @access Public (don't need a token)
-exports.getFreelancer = (req, res, next) => {
+exports.getFreelancer = async (req, res, next) => {
   // whatever we want to do go here
-  res.status(200).json({
+ try {
+   console.log(req.params)
+   const freelancer = await Freelancer.findById(req.params.id);
+
+   // need to check if the ID is in the DB // we use a return here so it'll break
+   if(!freelancer) {
+    return res.status(400).json({
+      success: false
+    });
+  
+   }
+
+   res.status(200).json({
     success: true,
-    msg: `Show freelancer ${req.params.id}.`
+    data: freelancer
   })
+
+ } catch (error) {
+  res.status(400).json({
+    success: false
+  });
+ }
 }
 
 // @desc Create new freelancer
@@ -35,7 +62,9 @@ exports.createFreelancer = async (req, res, next) => {
       data: freelancer
     })
   } catch (error) {
-    res.status(400).json({ success: false })
+    res.status(400).json({
+      success: false
+    })
   }
   // console.log(req.body);
   // res.status(200).json({
@@ -47,21 +76,55 @@ exports.createFreelancer = async (req, res, next) => {
 // @desc Update freelancer
 // @route PUT /api/v1/freelancers/:id
 // @access Private (need a token)
-exports.updateFreelancer = (req, res, next) => {
+exports.updateFreelancer = async (req, res, next) => {
   // whatever we want to do go here
-  res.status(200).json({
-    success: true,
-    msg: `Update freelancer ${req.params.id}.`
-  })
+  try {
+    const freelancer = await Freelancer.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, 
+      runValidators: true
+    });
+
+    if(!freelancer) {
+      return res.status(400).json({
+        success: false
+      });
+    }
+
+      res.status(200).json({
+        success: true,
+        data: freelancer
+      });
+    
+
+  } catch (error) {
+    res.status(400).json({
+      success: false
+    })
+  }
 }
 
 // @desc Delete freelancer
 // @route Delete /api/v1/freelancers/:id
 // @access Private (need a token)
-exports.deleteFreelancer = (req, res, next) => {
+exports.deleteFreelancer = async (req, res, next) => {
   // whatever we want to do go here
-  res.status(200).json({
-    success: true,
-    msg: `Delete freelancer ${req.params.id}.`
-  })
+  try {
+    const freelancer = await Freelancer.findByIdAndDelete(req.params.id);
+
+    if(!freelancer) {
+      return res.status(400).json({
+        success: false
+      });
+    }
+
+      res.status(200).json({
+        success: true,
+        data: {}
+      });
+    
+  } catch (error) {
+    res.status(400).json({
+      success: false
+    })
+  }
 }
