631769e7d3615a3ba45b07c6cba54ece062c3a70
Taylor Covington <tcovingtondev@gmail.com>
2020-04-06 17:39:20 -0600

    Added update password and sending link for forgotten password

--- models/User.model.js
+++ models/User.model.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
@@ -41,6 +42,9 @@ const UserSchema = new mongoose.Schema({
 
 // Encrype password usering bcrypt
 UserSchema.pre('save', async function (next) {
+  if(!this.isModified('password')) {
+    next();
+  }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt)
 });
@@ -58,8 +62,25 @@ UserSchema.methods.getSignedJwtToken = function () {
 
 // Match user entered to hashed password in database
 UserSchema.methods.matchPassword = async function (enteredPassword) {
-                                                // 'this' is the user from auth controller login
+  // 'this' is the user from auth controller login
   return await bcrypt.compare(enteredPassword, this.password)
+};
+
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function () {
+  // Generate token
+  const resetToken = crypto.randomBytes(20).toString('hex')
+
+  // Hash token and set to resetPasswordToken field
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  // Set expire to 10 minutes
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 }
 
 module.exports = mongoose.model('User', UserSchema);
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,12 +1,27 @@
 const express = require('express');
-const { register, login, getMe } = require('../controllers/auth.controller');
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword,
+  resetPassword,
+  updateDetails,
+  updatePassword
+} = require('../controllers/auth.controller');
 
 const router = express.Router();
 
-const { protect } = require('../middleware/auth.middleware')
+const {
+  protect
+} = require('../middleware/auth.middleware')
 
 router.post('/register', register);
 router.post('/login', login);
+router.post('/forgotpassword', forgotPassword);
+router.put('/resetpassword/:resettoken', resetPassword);
+//! Protected Routes
 router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 
 module.exports = router;
