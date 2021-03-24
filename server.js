e57ff31ad25e7d79b02e12679c4367d7e413534d
Taylor Covington <tcovingtondev@gmail.com>
2021-03-24 12:38:41 -0600

    updated cors and api request method

--- server.js
+++ server.js
@@ -75,7 +75,14 @@ app.use(hpp());
 
 //! Enable CORS
 
- app.use(cors());
+//  app.use(cors());
+app.all("*", function (req, res, next) {    
+  res.header("Access-Control-Allow-Origin", "*");    
+  res.header("Access-Control-Allow-Credentials", true);    
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");    
+  res.header("Access-Control-Allow-Headers", "Content-Type, *");    
+  next();   
+});
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
