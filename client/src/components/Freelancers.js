c2570e7f20ff3e8a5d39e5a7c0ac127d61143a63
Taylor Covington <tcovingtondev@gmail.com>
2021-04-06 21:16:23 -0600

    added axios and successfully requested data from server side db

--- client/src/components/Freelancers.js
+++ client/src/components/Freelancers.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import { useEffect } from 'react'
+import axios from '../axios'
 
 export const Freelancers = () => {
+
+  const getFreelancers = () => {
+    axios.get('/api/v1/freelancers')
+      .then((res) => {
+        console.log(res.data)
+      })
+  }
+
+  useEffect(() => {
+    getFreelancers()
+  }, [])
   return (
     <div>
       <h1>Freelancers</h1>
